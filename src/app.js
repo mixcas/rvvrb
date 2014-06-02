f347e972f8bde350799743f5ec5f7a9e2dcc3634
cas <casska@gmail.com>
2014-06-01 23:44:41 -0500

    Replace FFT for octave-band filters

--- src/app.js
+++ src/app.js
@@ -1,59 +1,92 @@
 // AudioContext
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
-var context = new AudioContext();
-var analyser = context.createAnalyser();
+var audioContext = new AudioContext();
+
+source = audioContext.createBufferSource();
+javascriptNode = audioContext.createScriptProcessor(256, 1, 1);
+
+javascriptNode.onaudioprocess = function() {
+  analyze();
+}
+
+source.connect(javascriptNode);
+
+// Create filters
+var filterFreqs = [
+  16,
+  31.5,
+  63,
+  125,
+  250,
+  500,
+  1000,
+  2000,
+  4000,
+  8000,
+  16000
+];
+
+var filters = [];
+var analysers = [];
+var timeByteData = [];
+soundLevelData = [];
+
+for ( var x = 0; x <= filterFreqs.length; x++ ) {
+
+  filters[x] = audioContext.createBiquadFilter();
+  filters[x].type = 2; //BANDPASS
+  filters[x].Q.value = 1.414214; //Q = 1.414214 for one octave bands
+  filters[x].frequency.value = filterFreqs[x];
+
+  analysers[x] = audioContext.createAnalyser();
+  analysers[x].fftSize = 256;
+
+  source.connect(filters[x]);
+  filters[x].connect(analysers[x]);
+  analysers[x].connect(audioContext.destination);
+
+  timeByteData[x] = new Uint8Array(analysers[x].frequencyBinCount);
+  soundLevelData[x] = new Uint8Array(analysers[x].frequencyBinCount);
+
+}
 
 //Read wav file
 function readWav(url) {
+  
   var request = new XMLHttpRequest();
   request.open('GET', url, true);
   request.responseType = 'arraybuffer';
 
   // Decode asynchronously
   request.onload = function() {
-    context.decodeAudioData(request.response, function(buffer) {
-      analyseWav(buffer);
-    },  function onError(e) {
-      console.log(e)
-    });
+    source.buffer = audioContext.createBuffer(request.response, false );
   }
+  
   request.send();
+
 }
 
-function analyseWav(buffer) {
-  
+function analyze() {
+  for ( var x = 0; x < filterFreqs.length; x++ ) {
+    analysers[x].getByteFrequencyData(timeByteData[x]);
+    soundLevelData[x] = rms(timeByteData[x]);
+  } 
+  console.log(soundLevelData[10]);  
 }
-// Analyse pcm
-function notanalyseWav(buffer) {
-  
-  var pcmData = new Array ( buffer.length ),
-      ampData = new Array();
-  
-  pcmData = buffer.getChannelData(0);
 
-  var vueltas = Math.floor(pcmData.length / (buffer.sampleRate / 1000 ) );
-
-  for( var i = 0; i <= vueltas; i++) {
-    var start = i * 1000;
-    var stop = start + 1000;
-    ampData[i] = 0;
-    for( var x =  start; x <= stop; x++ ) {
-      ampData[i] += Math.pow(pcmData[x],2);
-    }
-    ampData[i] = Math.sqrt(ampData[i]/1000);
+function rms(ary) {
+  var sum_of_squares = bigInt();
+  for( var x = 0; x < ary.length; x++) {
+    sum_of_squares += Math.pow(ary[x],2);
+    //console.log(sum_of_squares);
   }
-
-   
-  var ctx = document.getElementById("myChart").getContext("2d");
-  ctx.beginPath();
-
-  for( i = 0; i <= ampData.length; i++) {
-    ctx.lineTo(i,ampData[i]/100);
-  }
-
-  ctx.stroke();
-
+  
+  return Math.sqrt(sum_of_squares / ary.length);
 }
 
+function play() {
+  javascriptNode.connect(audioContext.destination);
+  source.start(0);
+}
 
-readWav('wav/1.wav');
+readWav('wav/2.wav');
